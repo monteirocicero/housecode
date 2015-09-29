5206d6575319e424f3f71806d9514765ff8811b3
Cicero Monteiro <cicerolmonteiro@gmail.com>
2015-09-29 15:47:30 -0300

    [CAP-4] #WIP - Listagem de livros

--- src/main/java/br/com/housecode/store/controllers/ProductsController.java
+++ src/main/java/br/com/housecode/store/controllers/ProductsController.java
@@ -5,6 +5,7 @@ import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.housecode.store.daos.ProductDAO;
@@ -13,22 +14,30 @@ import br.com.housecode.store.models.Product;
 
 @Controller
 @Transactional
+@RequestMapping("/products")
 public class ProductsController {
 	
 	@Autowired
 	private ProductDAO productDAO;
 
-	@RequestMapping("/products")
+	@RequestMapping(method = RequestMethod.POST)
 	public String save(Product product) {
 		productDAO.save(product);
 		System.out.println("Register the product " + product);
-		return "products/ok";
+		return "redirect:products";
 	}
 	
-	@RequestMapping("/products/form")
+	@RequestMapping("/form")
 	public ModelAndView form() {
 		ModelAndView modelAndView = new ModelAndView("products/form");
 		modelAndView.addObject("types", BookType.values());
+		return modelAndView;
+	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ModelAndView list() {
+		ModelAndView modelAndView = new ModelAndView("products/list");
+		modelAndView.addObject("products", productDAO.list());
 		return modelAndView;
 	}
 
