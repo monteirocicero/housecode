ecba927b8a5fc951950e1f6c7b4dd228f648a3a6
Cicero Monteiro <cicerolmonteiro@gmail.com>
2015-10-16 21:28:15 -0300

    [CAP-10] #DONE - Implementando view resolver para retornar JSON

--- src/main/java/br/com/housecode/store/conf/AppWebConfiguration.java
+++ src/main/java/br/com/housecode/store/conf/AppWebConfiguration.java
@@ -1,10 +1,11 @@
 package br.com.housecode.store.conf;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.cache.guava.GuavaCacheManager;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
@@ -14,10 +15,13 @@ import org.springframework.format.datetime.DateFormatter;
 import org.springframework.format.datetime.DateFormatterRegistrar;
 import org.springframework.format.support.DefaultFormattingConversionService;
 import org.springframework.format.support.FormattingConversionService;
+import org.springframework.web.accept.ContentNegotiationManager;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import com.google.common.cache.CacheBuilder;
@@ -26,6 +30,7 @@ import br.com.housecode.store.controllers.FileSaver;
 import br.com.housecode.store.controllers.HomeController;
 import br.com.housecode.store.daos.ProductDAO;
 import br.com.housecode.store.models.ShoppingCart;
+import br.com.housecode.store.viewresolver.JsonViewResolver;
 
 @EnableWebMvc
 @ComponentScan(basePackageClasses={HomeController.class, ProductDAO.class, FileSaver.class, ShoppingCart.class})
@@ -76,6 +81,19 @@ public class AppWebConfiguration {
 		GuavaCacheManager cacheManager = new GuavaCacheManager();
 		cacheManager.setCacheBuilder(builder);
 		return cacheManager;
+	}
+	
+	@Bean
+	public ViewResolver contentNegotiatingViewResolver(ContentNegotiationManager manager) {
+		List<ViewResolver> resolvers = new ArrayList<>();
+		
+		resolvers.add(internalResourceViewResolver());
+		resolvers.add(new JsonViewResolver());
+		
+		ContentNegotiatingViewResolver resolver = new ContentNegotiatingViewResolver();
+		resolver.setViewResolvers(resolvers);
+		resolver.setContentNegotiationManager(manager);
+		return resolver;
 	}
 
 }
--- src/main/java/br/com/housecode/store/controllers/ProductsController.java
+++ src/main/java/br/com/housecode/store/controllers/ProductsController.java
@@ -1,16 +1,18 @@
 package br.com.housecode.store.controllers;
 
+import java.util.List;
+
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
